3239443bb231094d87f2ed85b20eb73bd0532ddd
AngelRitsuka <88597612+AngelRitsuka@users.noreply.github.com>
2023-11-14 20:02:40 -0300

    Add files via upload

---
+++ localdb.js
@@ -0,0 +1,32 @@
+const db = {
+
+  para: { chunks: ['pa','ra'], phones: ['PA', 'RA']},
+  no: { chunks: [], phones: []},
+  um: { chunks: [], phones: []},
+  é: { chunks: [], phones: []},
+  que: { chunks: [], phones: []},
+  de: { chunks: [], phones: []},
+  ter: { chunks: [], phones: []},
+  na: { chunks: [], phones: []},
+  nome: { chunks: [], phones: []},
+  oi: { chunks: [], phones: []},
+  tudo: { chunks: [], phones: []},
+  bem: { chunks: [], phones: []},
+  bom: { chunks: [], phones: []},
+  sim: { chunks: [], phones: []},
+  não: { chunks: [], phones: []},
+  e: { chunks: [], phones: []},
+  você: { chunks: [], phones: []},
+  eu: { chunks: [], phones: []},
+  ele: { chunks: [], phones: []},
+  ela: { chunks: [], phones: []},
+  nós: { chunks: [], phones: []},
+  macaco: { chunks: [], phones: []},
+  macaquinho: { chunks: [], phones: []},
+  fofo: { chunks: [], phones: []},
+  um: { chunks: [], phones: []},
+  dois: { chunks: [], phones: []},
+
+
+};
+export default db;
